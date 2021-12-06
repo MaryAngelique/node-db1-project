274e0aed77e833fa8026da92d819d7a7d3be179c
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-06 17:54:51 -0500

    Created a middleware for checkAccountPayload which returns a 400 status if a request is invalid

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,6 +1,35 @@
+const Account = require("./accounts-model");
+const database = require("../../data/db-config");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const errorMessage = {status: 400}
+  const {name, budget} = req.body;
+
+  if ( name === undefined || budget === undefined ) {
+    errorMessage.message = "name and budget are required"
+  
+  } else if ( typeof name !== "string" ) {
+    errorMessage.message = "name of account must be a string"
+
+  } else if ( name.trim().length < 3 || name.trim().length > 100 ) {
+    errorMessage.message = "name of account must be between 3 and 100"
+
+  } else if ( typeof budget !== 'number' || isNaN(budget) ) {
+    errorMessage.message = "/budget of account must be a number/i"
+
+  } else if ( budget < 0 || budget > 1000000 ) {
+    errorMessage.message = "budget of account is too large or too small"
+  }
+
+  if ( errorMessage.message ){
+    next( errorMessage );
+
+  }else{
+    next();
+  }
 }
+
+
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
